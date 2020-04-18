d0a0ca12630035ae405fa6a14166d4c12aec839c
Rafael Gavarron <rafael.gavaron@gmail.com>
2020-04-18 16:23:42 -0300

    traços adicionados

--- src/LacosEncadeados.java
+++ src/LacosEncadeados.java
@@ -3,7 +3,7 @@ public class LacosEncadeados {
         for (int multiplicador = 1; multiplicador <= 10; multiplicador++) {
             for (int contador = 0; contador <= 10; contador++) {
                 System.out.print (multiplicador * contador);
-                System.out.print (" ");
+                System.out.print (" --- ");
             }
             System.out.println ();
         }
