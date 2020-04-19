9b30f65356705e52c6d1def2d1d083d9190a1067
Rafael Gavarron <rafael.gavaron@gmail.com>
2020-04-19 17:53:52 -0300

    Desafio concluido

--- src/DesafioJava.java
+++ src/DesafioJava.java
@@ -1,12 +1,11 @@
 public class DesafioJava {
     public static void main(String[] args) {
-        int numero = 3;
-        while (true) {
-            if (numero % 3 != 0) break;
-            if (numero > 100) {
-                break;
+        for (int numero = 1; numero % 3 != 0; numero++) {
+            for (int numero1 = 0; numero1 % 3 == 0; numero1 = numero1 + 3) {
+                if (numero1 < 100) {
+                    System.out.println (numero1);
+                }
             }
-            System.out.println (numero);
         }
     }
 }
